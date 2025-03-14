24e639e6907686015153e0678bbd828a1f1de0ca
Nguyen Trong <>
2025-03-14 18:03:53 +0700

    install mui into frontend project

---
+++ front-end/src/app/product/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@mui/material";
+
+function ProductPage() {
+  return (
+    <div>
+      <Button variant="contained">demo</Button>
+    </div>
+  );
+}
+
+export default ProductPage;
